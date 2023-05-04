b43ff01c530e52461893d78496528bc5f9ed1724
Bilkish-akther <121663376+Bilkish-akther@users.noreply.github.com>
2023-05-04 08:22:31 -0700

    Add files via upload
    exesting some code

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const cors=require('cors');
 const port = process.env.PORT || 5000;
-
+const serviceData=require('./data.json')
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Bangladeshi Foods recipe is Running')
-  });
+  })
 
-app.get('/chep', (req, res) => {
-    res.send('')
+  app.get('/recipe', (req, res) => {
+    res.send(serviceData);
   })
 
   app.listen(port, () => {
